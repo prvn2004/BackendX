465c4efd63f61b6c22dbc752436197df3e123015
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2024-12-04 00:09:41 +0530

    test changes

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const userRoutes = require('./routes/userRoutes');
 const { Server } = require('socket.io');
 const { createServer } = require('node:http');
 const newMessage = require('./controllers/newMessage');
-const {initSockets} = require('./controllers/commonFunctions/Socket/initSockets');
+const { initSockets } = require('./controllers/commonFunctions/Socket/initSockets');
 const cors = require('cors');
 
 const server = createServer(app);
 const io = new Server(server);
+
+// Middleware to log all API requests
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 
 mongoose.connect("mongodb+srv://prvn:prvn2004@mernapp.wh18ryw.mongodb.net/?retryWrites=true&w=majority")
     .then(() => {
@@ -24,6 +30,19 @@ app.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
+// Test GET endpoint with mock data
+app.get('/mock', (req, res) => {
+    res.json({
+        message: "This is a mock response",
+        success: true,
+        data: {
+            id: 1,
+            name: "Mock Data",
+            description: "This is some mock data for testing."
+        }
+    });
+});
+
 app.use(cors());
 app.use('/user', userRoutes);
 app.use('/chat', require('./routes/chatRoutes'));
@@ -35,7 +54,7 @@ app.use('/followup', require('./routes/followupRoutes'));
 app.use('/web', require('./routes/webRoutes'));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('A user connected');
 
     initSockets(socket, io);
 });
